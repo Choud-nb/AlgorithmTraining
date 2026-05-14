dd168a94de940847fd8b7b812bc61e113eed590f
Choud-nb <choudzhou@gmail.com>
2026-05-14 19:24:14 +0800

    优化并确定readme结构

---
+++ 1.入门阶段/P1421.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>a>>b;
+    cout<<(a*10+b)/19<<"\n";
+    return 0;
+}
+
+//AC
+//summit 1
